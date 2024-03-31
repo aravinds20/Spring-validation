bba0903f93adcd9bbd73f919e648371fa5d4b7be
aravind <aravinds.cb@gmail.com>
2024-03-31 18:39:13 +0530

    Added pom.xml

---
+++ validatons/src/main/java/com/aravind/springdemo/validatons/ValidatonsApplication.java
@@ -0,0 +1,13 @@
+package com.aravind.springdemo.validatons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatonsApplication.class, args);
+	}
+
+}
